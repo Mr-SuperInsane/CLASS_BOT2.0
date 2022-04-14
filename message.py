d164fa82d05f17053dcc1888eae224375a56b121
Naokun <70059960+Nao-kun81@users.noreply.github.com>
2022-04-15 06:33:07 +0900

    Update message.py

--- message.py
+++ message.py
@@ -7,9 +7,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Gspread処理
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('class-bot-347215-a4b263ffe581.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('GCP鍵jsonファイル', scope)
 gc = gspread.authorize(credentials)
-SPREADSHEET_KEY = '128nXuzlxRSmhm7P1g0-tQC9rl3MSdVcHLF_2vp0ehpI'
+SPREADSHEET_KEY = 'スプレッドシートID'
 worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
 
 #時間割
